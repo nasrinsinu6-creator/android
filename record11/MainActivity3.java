e204ee2fb7e879c916999e5e5ef66f24965f0332
nasrinsinu6-creator <nasrinsinu6@gmail.com>
2026-09-23 12:25:51 +0530

    Add MainActivity3 with basic setup and layout

---
+++ record11/MainActivity3.java
@@ -0,0 +1,24 @@
+package com.example.intent;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity3 extends AppCompatActivity {
+    TextView t1;
+;    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_third);
+        t1=findViewById(R.id.textView5);
+    }
+
+}
