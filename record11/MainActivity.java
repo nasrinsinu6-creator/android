ec654c1ef428cf22c11687771ea4b95fe76431ab
nasrinsinu6-creator <nasrinsinu6@gmail.com>
2026-09-23 12:24:55 +0530

    Create MainActivity with intent data passing
    Implement MainActivity with button click to start MainActivity2 and pass data.

---
+++ record11/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    b=findViewById(R.id.button);
+    }
+         public void Click(View view){
+            Intent i=new Intent(this, MainActivity2.class);
+            i.putExtra("name","Sinu");
+            i.putExtra("age","22");
+            i.putExtra("place","Karanthur");
+            i.putExtra("college","KMCT");
+
+
+             startActivity(i);
+        }
+    }
